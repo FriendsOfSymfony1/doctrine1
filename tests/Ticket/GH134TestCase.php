606ef02d9ee7d2c13cb72ecdaeb7722f21281a83
Alexandre Quercia <alquerci@email.com>
2024-04-16 21:52:03 +0200

    fixup! Add test to characterize previous behaviour and avoid regression

--- tests/Ticket/GH134TestCase.php
+++ tests/Ticket/GH134TestCase.php
@@ -2,112 +2,76 @@
 
 class Doctrine_Ticket_GH134_TestCase extends Doctrine_UnitTestCase
 {
-    public function test_hydrateArray_identifierOfRelations()
+    public function test_identifierAndRelation()
     {
-        $query = Doctrine_Query::create()
-            ->select('u.id, e.address as aliasAddress')
-            ->from('User u')
-            ->innerJoin('u.Email e')
-        ;
+        foreach ($this->provideIdentifierAndRelationData() as [$hydrateType, $expectedSql, $expectedFirstResult]) {
+            $query = Doctrine_Query::create()
+                ->select('u.id, e.address as aliasAddress')
+                ->from('User u')
+                ->innerJoin('u.Email e')
+            ;
 
-        $results = $query->execute(array(), Doctrine_Core::HYDRATE_ARRAY);
+            $results = $query->execute(array(), $hydrateType);
 
-        $expectedSql = 'SELECT e.id AS e__id, e2.id AS e2__id, e2.address AS e2__0 FROM entity e INNER JOIN email e2 ON e.email_id = e2.id WHERE (e.type = 0)';
+            $this->assertEqual($expectedSql, $query->getSqlQuery());
 
-        $this->assertEqual($expectedSql, $query->getSqlQuery());
+            $this->assertEqual($expectedFirstResult, $results[0]);
+            $this->assertEqual(count($this->users), count($results));
+        }
+    }
 
-        $expected = array (
-            'id' => '4',
-            'aliasAddress' => 'zYne@example.com',
-            'Email' => array (
-                'id' => 1,
+    private function provideIdentifierAndRelationData()
+    {
+        yield [
+            Doctrine_Core::HYDRATE_ARRAY,
+            'SELECT e.id AS e__id, e2.id AS e2__id, e2.address AS e2__0 FROM entity e INNER JOIN email e2 ON e.email_id = e2.id WHERE (e.type = 0)',
+            array (
+                'id' => '4',
+                'aliasAddress' => 'zYne@example.com',
+                'Email' => array (
+                    'id' => 1,
+                    'aliasAddress' => 'zYne@example.com',
+                ),
+            ),
+        ];
+
+        yield [
+            Doctrine_Core::HYDRATE_ARRAY_SHALLOW,
+            'SELECT e.id AS e__id, e2.address AS e2__0 FROM entity e INNER JOIN email e2 ON e.email_id = e2.id WHERE (e.type = 0)',
+            array (
+                'id' => '4',
                 'aliasAddress' => 'zYne@example.com',
             ),
-        );
+        ];
 
-        $this->assertEqual($expected, $results[0]);
-        $this->assertEqual(count($this->users), count($results));
+        yield [
+            Doctrine_Core::HYDRATE_SCALAR,
+            'SELECT e.id AS e__id, e2.address AS e2__0 FROM entity e INNER JOIN email e2 ON e.email_id = e2.id WHERE (e.type = 0)',
+            array (
+                'u_id' => '4',
+                'e_aliasAddress' => 'zYne@example.com',
+            ),
+        ];
     }
 
-    public function test_hydrateArrayShallow_identifierOfRelations()
+    public function test_columnAliasWithSameNameAsIdentifier_willKeepOverwriteIdentifierByAlias()
     {
-        $query = Doctrine_Query::create()
-            ->select('u.id, e.address as aliasAddress')
-            ->from('User u')
-            ->innerJoin('u.Email e')
-        ;
+        foreach ($this->provideKeepOverwriteIdentifierWithAliasData() as [$hydrateType, $checkArrayIndex]) {
+            $query = Doctrine_Query::create()
+                ->select('99 as id, u.id as aliasId')
+                ->from('User u')
+            ;
 
-        $results = $query->execute(array(), Doctrine_Core::HYDRATE_ARRAY_SHALLOW);
+            $results = $query->execute(array(), $hydrateType);
 
-        $expectedSql = 'SELECT e.id AS e__id, e2.address AS e2__0 FROM entity e INNER JOIN email e2 ON e.email_id = e2.id WHERE (e.type = 0)';
-
-        $this->assertEqual($expectedSql, $query->getSqlQuery());
-
-        $expected = array (
-            'id' => '4',
-            'aliasAddress' => 'zYne@example.com',
-        );
-
-        $this->assertEqual($expected, $results[0]);
-        $this->assertEqual(count($this->users), count($results));
+            $this->assertEqual(99, $results[0][$checkArrayIndex]);
+        }
     }
 
-    public function test_hydrateScalar_identifierOfRelations()
+    private function provideKeepOverwriteIdentifierWithAliasData()
     {
-        $query = Doctrine_Query::create()
-            ->select('u.id, e.address as aliasAddress')
-            ->from('User u')
-            ->innerJoin('u.Email e')
-        ;
-
-        $results = $query->execute(array(), Doctrine_Core::HYDRATE_SCALAR);
-
-        $expectedSql = 'SELECT e.id AS e__id, e2.address AS e2__0 FROM entity e INNER JOIN email e2 ON e.email_id = e2.id WHERE (e.type = 0)';
-
-        $this->assertEqual($expectedSql, $query->getSqlQuery());
-
-        $expected = array (
-            'u_id' => '4',
-            'e_aliasAddress' => 'zYne@example.com',
-        );
-
-        $this->assertEqual($expected, $results[0]);
-        $this->assertEqual(count($this->users), count($results));
-    }
-
-    private function doTestAliasWithSameNameAsIdentifiers($hydrateType, $checkArrayIndex)
-    {
-        $query = Doctrine_Query::create()
-            ->select('99 as id, u.id as aliasId')
-            ->from('User u')
-        ;
-
-        $results = $query->execute(array(), $hydrateType);
-
-        $this->assertEqual(99, $results[0][$checkArrayIndex]);
-    }
-
-    public function test_hydrateScalar_aliasWithSameNameAsIdentifiers_willKeepOverwriteIdentifierByAlias()
-    {
-        $hydrateType = Doctrine_Core::HYDRATE_SCALAR;
-        $checkArrayIndex = 'u_id';
-
-        $this->doTestAliasWithSameNameAsIdentifiers($hydrateType, $checkArrayIndex);
-    }
-
-    public function test_hydrateArrayShallow_aliasWithSameNameAsIdentifiers_willKeepOverwriteIdentifierByAlias()
-    {
-        $hydrateType = Doctrine_Core::HYDRATE_ARRAY_SHALLOW;
-        $checkArrayIndex = 'id';
-
-        $this->doTestAliasWithSameNameAsIdentifiers($hydrateType, $checkArrayIndex);
-    }
-
-    public function test_hydrateArray_aliasWithSameNameAsIdentifiers_willKeepOverwriteIdentifierByAlias()
-    {
-        $hydrateType = Doctrine_Core::HYDRATE_ARRAY;
-        $checkArrayIndex = 'id';
-
-        $this->doTestAliasWithSameNameAsIdentifiers($hydrateType, $checkArrayIndex);
+        yield [Doctrine_Core::HYDRATE_SCALAR, 'u_id'];
+        yield [Doctrine_Core::HYDRATE_ARRAY, 'id'];
+        yield [Doctrine_Core::HYDRATE_ARRAY_SHALLOW, 'id'];
     }
 }
